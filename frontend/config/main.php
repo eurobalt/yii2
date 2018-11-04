1c1b0eefb0ddc9cead5b9d98177b41eac83a14cf
Eurobalt <vladislav7able@gmail.com>
2018-11-04 11:24:02 +0200

    Commit after github problems

---
+++ frontend/config/main.php
@@ -0,0 +1,90 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-frontend',
+    'language' => 'ru',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'StringHelper' => [
+            'class' => 'frontend\components\StringHelper'
+        ],
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'stringHelper' => [
+            'class' => 'frontend\components\StringHelper',
+        ],
+       
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => true,
+          
+            'rules' => [
+
+                // NewsController
+                'novosti' => 'news/index',
+                'novosti/<id:\d+>' => 'news/article',
+
+                // SiteController
+                'subscribe' => 'site/subscribe',
+                '' => 'site/index',
+            ],
+        ],
+        
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.beget.com',
+                'username' => 'support@xenforo.ws',
+                'password' => 'ellada27',
+                'port' => '2525',
+                'encryption' => '',
+            ],
+        ],
+        'request' => [
+            'baseUrl' => ''
+        ],
+        
+        
+        
+        
+    ],
+    'params' => $params,
+];
